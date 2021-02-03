c9fefb868a0ae076a4d4877319906618961224f4
Alice <www@mail.ru>
2021-02-03 17:05:49 +0200

    Add

---
+++ Add_js_file.js
@@ -0,0 +1,10 @@
+
+To web part script editor:
+<script src="/_catalogs/masterpage/3shape/scripts/   .js"></script> 
+<div id="documentBloks"></div>
+
+To js file:
+$(document).ready(function() {
+    $("#documentBloks").append("\
+    ");
+});
